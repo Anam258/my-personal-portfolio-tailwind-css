341c7379f357619222ab5270d33c1eddea4b93da
Anam258 <58658777+Anam258@users.noreply.github.com>
2024-11-10 01:37:00 +0500

    Update Skills.js

--- src/app/components/Skills.js
+++ src/app/components/Skills.js
@@ -3,7 +3,7 @@ import { FaReact, FaNodeJs, FaHtml5, FaCss3Alt } from "react-icons/fa";
 export default function Skills() {
   return (
     <div className="container mx-auto py-16 px-4 text-center">
-      <h2 className="text-3xl font-bold mb-8">My Skills</h2>
+
       <div className="flex flex-wrap justify-center gap-8">
         <div className="flex flex-col items-center w-32">
           <FaReact className="text-6xl text-blue-600 mb-4" />
